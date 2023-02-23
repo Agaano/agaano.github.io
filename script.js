4f2218ea433ac6b24b5f3241c78f928bc2c4b7cb
Agaano <121362942+Agaano@users.noreply.github.com>
2023-02-23 23:57:40 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
+
 let addition = 25;
 let obj = document.getElementsByClassName("img");
 let intId = [obj.length];
 const objWidth = Number(getComputedStyle(obj[0]).width.slice(0,-2).valueOf());
 const objHeight = Number(getComputedStyle(obj[0]).height.slice(0,-2).valueOf());
+
 if (window.screen.height < window.screen.width)
 {
-    for (let i = 0; i < obj.length; i++) {
-        obj[i].addEventListener('mouseenter', () => blockanimon(obj[i],i));
-        obj[i].addEventListener('mouseleave', () => blockanimoff(obj[i],i));
-    }
+    for (let i = 0; i < obj.length; i++)
+        {
+
+            obj[i].addEventListener('mouseenter', () => blockanimon(obj[i],i));
+            obj[i].addEventListener('mouseleave', () => blockanimoff(obj[i],i));
+        }
 }
 else {   
     for (let i = 0; i < obj.length; i++) {
